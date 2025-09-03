57c0c82d1297d15b782c4d568f1ab01b0a15ebe9
Neustod <vane90989@gmail.com>
2025-09-04 00:41:03 +0300

    fix: bundle of balloonsgit add Localization/en-US_Mods.Overclocked.hjson Reworks/BundleOfBalloonsRework.cs git add Localization/en-US_Mods.Overclocked.hjson Reworks/BundleOfBalloonsRework.cs ! Yaaaaygit add Localization/en-US_Mods.Overclocked.hjson Reworks/BundleOfBalloonsRework.cs  >:3

--- Reworks/BundleOfBalloonsRework.cs
+++ Reworks/BundleOfBalloonsRework.cs
@@ -12,103 +12,26 @@ namespace Overclocked.Reworks
     {
         public override void AddRecipeGroups()
         {
-            RecipeGroup EvilBossMaterial = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.ShadowScale)}",ItemID.ShadowScale, ItemID.TissueSample);
+            RecipeGroup EvilBossMaterial = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.ShadowScale)}", ItemID.ShadowScale, ItemID.TissueSample);
             RecipeGroup.RegisterGroup(nameof(ItemID.ShadowScale), EvilBossMaterial);
         }
         public override void AddRecipes()
         {
-            for (int i = 0; i < Main.recipe.Length; i++)
-            {
-                Recipe recipe = Main.recipe[i];
-                if (recipe.createItem.type == ItemID.BundleofBalloons || recipe.createItem.type == ItemID.HorseshoeBundle)
-                {
-                    recipe.DisableRecipe();
-                }
-            }
-            Recipe BundleofBalloons = Recipe.Create(ItemID.BundleofBalloons);
-            BundleofBalloons.AddIngredient(ItemID.CloudinaBalloon);
-            BundleofBalloons.AddIngredient(ItemID.BlizzardinaBalloon);
-            BundleofBalloons.AddIngredient(ItemID.SandstorminaBalloon);
-            BundleofBalloons.AddRecipeGroup(nameof(ItemID.ShadowScale), 5);
-            BundleofBalloons.AddTile(TileID.TinkerersWorkbench);
-            BundleofBalloons.Register();
-            Recipe HorseshoeBundleBundle = Recipe.Create(ItemID.HorseshoeBundle);
-            HorseshoeBundleBundle.AddIngredient(ItemID.BundleofBalloons);
-            HorseshoeBundleBundle.AddIngredient(ItemID.LuckyHorseshoe);
-            HorseshoeBundleBundle.AddTile(TileID.TinkerersWorkbench);
-            HorseshoeBundleBundle.Register();
-            Recipe HorseshoeBundleAll = Recipe.Create(ItemID.HorseshoeBundle);
-            HorseshoeBundleAll.AddIngredient(ItemID.CloudinaBalloon);
-            HorseshoeBundleAll.AddIngredient(ItemID.BlizzardinaBalloon);
-            HorseshoeBundleAll.AddIngredient(ItemID.SandstorminaBalloon);
-            HorseshoeBundleAll.AddIngredient(ItemID.LuckyHorseshoe);
-            HorseshoeBundleAll.AddRecipeGroup(nameof(ItemID.ShadowScale), 5);
-            HorseshoeBundleAll.AddTile(TileID.TinkerersWorkbench);
-            HorseshoeBundleAll.Register();
-            Recipe HorseshoeBundleCloud = Recipe.Create(ItemID.HorseshoeBundle);
-            HorseshoeBundleCloud.AddIngredient(ItemID.BlueHorseshoeBalloon);
-            HorseshoeBundleCloud.AddRecipeGroup(RecipeGroupID.BlizzardBalloons);
-            HorseshoeBundleCloud.AddRecipeGroup(RecipeGroupID.SandstormBalloons);
-            HorseshoeBundleCloud.AddRecipeGroup(nameof(ItemID.ShadowScale), 5);
-            HorseshoeBundleCloud.AddTile(TileID.TinkerersWorkbench);
-            HorseshoeBundleCloud.Register();
-            Recipe HorseshoeBundleBlizzard = Recipe.Create(ItemID.HorseshoeBundle);
-            HorseshoeBundleBlizzard.AddIngredient(ItemID.WhiteHorseshoeBalloon);
-            HorseshoeBundleBlizzard.AddRecipeGroup(RecipeGroupID.CloudBalloons);
-            HorseshoeBundleBlizzard.AddRecipeGroup(RecipeGroupID.SandstormBalloons);
-            HorseshoeBundleBlizzard.AddRecipeGroup(nameof(ItemID.ShadowScale), 5);
-            HorseshoeBundleBlizzard.AddTile(TileID.TinkerersWorkbench);
-            HorseshoeBundleBlizzard.Register();
-            Recipe HorseshoeBundleSandstorm = Recipe.Create(ItemID.HorseshoeBundle);
-            HorseshoeBundleSandstorm.AddIngredient(ItemID.YellowHorseshoeBalloon);
-            HorseshoeBundleSandstorm.AddRecipeGroup(RecipeGroupID.BlizzardBalloons);
-            HorseshoeBundleSandstorm.AddRecipeGroup(RecipeGroupID.CloudBalloons);
-            HorseshoeBundleSandstorm.AddRecipeGroup(nameof(ItemID.ShadowScale), 5);
-            HorseshoeBundleSandstorm.AddTile(TileID.TinkerersWorkbench);
-            HorseshoeBundleSandstorm.Register();
             // Removing Recipes.
-            //   List<Recipe> rec = Main.recipe.ToList();
-            //   int numberRecipesRemoved = 0;
-            // The Recipes to remove.
-            //numberRecipesRemoved += rec.RemoveAll(x => x.createItem.type == ItemID.AlphabetStatueU);
+            List<Recipe> rec = Main.recipe.ToList();
+            //
             //Change AlphabetStatueT's recipe to create 10 clentaminator from 10 wood
-            //   rec.Where(x => x.createItem.type == ItemID.HorseshoeBundle  /*|| x.createItem.type == ItemID.HorseshoeBundle*//* || x.requiredItem == ItemID.HorseshoeBundle*/).ToList().ForEach(s =>
-            //   {
-            //for (int i = 0; i < s.requiredItem.Length; i++)
-            //{
-            //    s.requiredItem[i] = new Item();
-            //}
-            //if (rec.requiredItem == ItemID.HorseshoeBundle) { }
-            //       s.AddRecipeGroup(nameof(ItemID.ShadowScale), 1);
-            //s.requiredItem[12].SetDefaults("Overclocked:ShadowScale", false);
-            //s.requiredItem[12].stack = 10;
-
-            //s.createItem.SetDefaults(ItemID.Clentaminator, false);
-            //s.createItem.stack = 10;
-            /*foreach (Recipe recipe in Main.recipe)
+            rec.Where(x => 
+                (x.createItem.type == ItemID.HorseshoeBundle || 
+                x.createItem.type == ItemID.BundleofBalloons) &&
+                !x.requiredItem.Any(x => x.type == ItemID.BundleofBalloons)
+            ).ToList().ForEach(s =>
             {
-                foreach (Item item in recipe.requiredItem)
-                {
-                    if (item.type != ItemID.BundleofBalloons) // Check for Bundle of Balloons
-                    {
-                        // Do something, like logging or modifying the recipe
-                        s.AddRecipeGroup(nameof(ItemID.ShadowScale), 1);
-                    }
-                }
-            }*/
-            //  });
-            //  Main.recipe = rec.ToArray();
-            //  Array.Resize(ref Main.recipe, Recipe.maxRecipes);
-            //  Recipe.numRecipes -= numberRecipesRemoved;
-
-            /*Recipe recipe = Recipe.Create(ItemID.BundleofBalloons);
-            recipe.AddIngredient(ItemID.CloudinaBalloon);
-            recipe.AddIngredient(ItemID.BlizzardinaBalloon);
-            recipe.AddIngredient(ItemID.SandstorminaBalloon);
-            recipe.AddIngredient(ItemID.SandstorminaBalloon);
-            recipe.AddRecipeGroup(RecipeGroupID.);
-            recipe.AddTile(TileID.TinkerersWorkbench);
-            recipe.Register();*/
+                s.AddRecipeGroup(nameof(ItemID.ShadowScale), 1);
+            });
+            //
+            Main.recipe = rec.ToArray();
+            //Array.Resize(ref Main.recipe, Recipe.maxRecipes);
         }
     }
 }
