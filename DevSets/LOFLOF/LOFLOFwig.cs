aa5f7a9ae21de95c1377ed53ace2f56616dd4f20
ŁOFŁOF <2010.adas.e@gmail.com>
2025-09-20 22:17:47 +0200

    FIxed the namespace in LOFLOF dev set

--- DevSets/LOFLOF/LOFLOFwig.cs
+++ DevSets/LOFLOF/LOFLOFwig.cs
@@ -2,7 +2,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace LOFLOFdevSet
+namespace Overclocked.DevSets.LOFLOF
 {
     // This tells tModLoader to look for a texture called MinionBossMask_Head, which is the texture on the player
     // and then registers this item to be accepted in head equip slots
